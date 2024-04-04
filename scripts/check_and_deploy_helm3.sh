afcbf47b237e1b8c03b4c719b0377fde8e98b0f5
jed <jed@HME-Pro.local>
2024-04-05 01:28:03 +0800

    allow unsupported workers

--- scripts/check_and_deploy_helm3.sh
+++ scripts/check_and_deploy_helm3.sh
@@ -90,12 +90,16 @@ echo "=========================================================="
 echo "CHECKING CLUSTER readiness and namespace existence"
 if [ -z "${KUBERNETES_MASTER_ADDRESS}" ]; then
   CLUSTER_ID=${PIPELINE_KUBERNETES_CLUSTER_ID:-${PIPELINE_KUBERNETES_CLUSTER_NAME}} # use cluster id instead of cluster name to handle case where there are multiple clusters with same name
-  IP_ADDR=$( ibmcloud ks workers --cluster ${CLUSTER_ID} |  head -n 1 | awk '{ print $2 }' )
+  IP_ADDR=$( ibmcloud ks workers --cluster ${CLUSTER_ID} | grep normal | head -n 1 | awk '{ print $2 }' )
   if [ -z "${IP_ADDR}" ]; then
-    echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
-    exit 1
+    IP_ADDR=$( ibmcloud ks workers --cluster ${CLUSTER_ID} | grep unsupported | head -n 1 | awk '{ print $2 }' )
+    if [ -z "${IP_ADDR}" ]; then
+      echo -e "${PIPELINE_KUBERNETES_CLUSTER_NAME} not created or workers not ready"
+      exit 1
+    fi
   fi
 fi
+
 echo "Configuring cluster namespace"
 if kubectl get namespace ${CLUSTER_NAMESPACE}; then
   echo -e "Namespace ${CLUSTER_NAMESPACE} found."
